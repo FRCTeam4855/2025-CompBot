3c36141f867a355753819f9cb5c7bd060b3a2e11
JacobJK21 <jacob.keramzcak@outlook.com>
2026-01-31 13:37:49 -0500

    2026 Update

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -2,9 +2,9 @@ package frc.robot;
 
 import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.FeedbackSensor;
 import frc.robot.Constants.ModuleConstants;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public final class Configs {
 
